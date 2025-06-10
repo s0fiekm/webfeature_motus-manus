f5aa1bf71cb03a362c875b3f2c4faca89ae5f83f
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2025-06-10 21:27:32 +0200

    config-css

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <main className="min-h-screen flex items-center justify-center bg-green-100">
+     
+    </main>
+  )
+}
+  
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        colors: {
+            primary: '#646D55',
+            secondary: '#EAE6DB',
+            tertiary: '#BCC1AE',
+            surface: '#D0C8B4',  
+            cta: '#DE774D',
+            error: '#E57373',
+        },
+        fontFamily: {
+            primary: ['skeena', 'sans-serif'],
+        }
+    },
+  },
+  plugins: [],
+}
