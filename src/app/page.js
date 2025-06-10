07c427591164d157a9b7a376a29e0786f019276c
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2025-06-10 21:52:50 +0200

    logo, layout, ikoner

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-
-    <main className="min-h-screen flex items-center justify-center bg-green-100">
+<div>
+  <Header/>
+    <main className="min-h-screen flex items-center justify-center">
      
-    </main>
+    </main></div>
   )
 }
   
