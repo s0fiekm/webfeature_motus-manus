67711a658c3acf09422af71a9ec7337f163f59b9
s0fiekm <151931942+s0fiekm@users.noreply.github.com>
2025-06-10 23:27:01 +0200

    global css

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="min-h-screen font-primary bg-secondary text-primary">
+      <body className="min-h-screen ">
         <Header/>
         <main>{children}</main>
         {/* Din Footer */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,24 @@ module.exports = {
   './pages/**/*.{js,ts,jsx,tsx}',
   './src/components/**/*.{js,ts,jsx,tsx}',
   './src/**/*.{js,ts,jsx,tsx}',
+  './app/**/*.{js,ts,jsx,tsx}', 
   ],
   theme: {
     extend: {
         colors: {
-            primary: '#646D55',
-            secondary: '#EAE6DB',
-            tertiary: '#BCC1AE',
-            surface: '#D0C8B4',  
-            cta: '#DE774D',
-            error: '#E57373',
+            "primary": '#646D55',
+            "secondary": '#EAE6DB',
+            "tertiary": '#BCC1AE',
+            "surface": '#D0C8B4',  
+            "cta": '#DE774D',
+            "error": '#E57373',
         },
         fontFamily: {
             primary: ['skeena', 'sans-serif'],
-        }
+        },
+       fontSize: {
+        'logo': '2.5rem',
+       },
     },
   },
   plugins: [],
